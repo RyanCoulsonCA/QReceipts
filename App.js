df0a5f83e0bed0f019909e3f4b8b7f99cdd65507
Ryan Coulson <contact@ryan-coulson.com>
2019-08-24 12:22:04 -0400

    add header bar; set up FontAwesome vector icons

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+class Header extends Component {
+  render() {
+    return (
+      <View style={this.props.style}>
+        <Icon style={styles.headerIcon} name="bars" />
+        <Text style={styles.headerLogo}>QReceipt</Text>
+        <Icon style={styles.headerIcon} name="camera" />
+      </View>
+    );
+  }
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <Header style={styles.header} />
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
     </View>
   );
 }
@@ -12,8 +28,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 15
   },
+  header: {
+    backgroundColor: "#F5F5F5",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 40,
+    zIndex: 1,
+
+    shadowRadius: 3,
+    shadowOpacity: 0.3,
+    shadowOffset: {
+      height: 2,
+      width: 0
+    }
+  },
+  headerLogo: {
+    flex: 80,
+    textAlign: "center",
+    color: "#AAAAAA",
+    letterSpacing: 1
+  },
+  headerIcon: {
+    flex: 10,
+    textAlign: "center",
+    fontSize: 16,
+    color: "#AAAAAA"
+  }
 });
